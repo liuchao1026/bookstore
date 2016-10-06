0ecb75a0cf088abb0c4c8b0d80d37b7c7e1d2c5a
liuchao1026 <734362851@qq.com>
2016-10-06 16:27:09 +0800

    .......

--- bookstore-web/src/main/java/com/hzit/StartSpring.java
+++ bookstore-web/src/main/java/com/hzit/StartSpring.java
@@ -1,7 +1,28 @@
 package com.hzit;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
- * Created by Administrator on 2016/10/6.
+ * Created by Administrator on 2016/9/26.
  */
-public class StartSpring {
+@SpringBootApplication
+@ComponentScan({"com.fc,com.hzit.service,com.hzit.controller"})
+@MapperScan("com.hzit.dao.mapper")
+public class StartSpring extends SpringBootServletInitializer {
+//    public StartSpring(){
+//        System.out.println("servlet_1类被创建了咯");
+//    }
+//
+//    public void test1(){
+//        System.out.println("这是我用springBoot框架写的第一个方法咯");
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpring.class,args);
+    }
+
 }
